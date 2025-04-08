0c40a65a5b3573e2c5756ecf1d7a0183db8e2cd5
CRI USER <sandragjacinto@gmail.com>
2025-04-08 15:27:35 +0200

    handles tests upadtes

--- src/views/__tests__/TutorPage.test.ts
+++ src/views/__tests__/TutorPage.test.ts
@@ -1,8 +1,12 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { shallowMount } from '@vue/test-utils';
 import TutorPage from '../TutorPage.vue';
+import { createPinia, setActivePinia } from 'pinia';
 
 describe('TutorPage', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
   it('renders properly', () => {
     const wrapper = shallowMount(TutorPage);
     expect(wrapper.element).toMatchSnapshot();
