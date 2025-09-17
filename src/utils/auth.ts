ea2fb4648d883631c18182444d9c1d08b7e7f2d0
CRI USER <sandragjacinto@gmail.com>
2025-09-17 17:15:22 +0200

    fix: wrap auth in try catch

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,14 +1,18 @@
 import { postAxios } from '@/utils/fetch';
 
 export const getUserAndSession = async () => {
-  const userID = localStorage.getItem('userId');
-  const sessionID = localStorage.getItem('sessionId');
+  try {
+    const userID = localStorage.getItem('userId');
+    const sessionID = localStorage.getItem('sessionId');
 
-  const newUserId = await postAxios(`/user/user?user_id=${userID || ''}`);
-  const newSessionId = await postAxios(
-    `/user/session?user_id=${newUserId.data.user_id || ''}&session_id=${sessionID || ''}`
-  );
+    const newUserId = await postAxios(`/user/user?user_id=${userID || ''}`);
+    const newSessionId = await postAxios(
+      `/user/session?user_id=${newUserId.data.user_id || ''}&session_id=${sessionID || ''}`
+    );
 
-  localStorage.setItem('userId', newUserId.data.user_id);
-  localStorage.setItem('sessionId', newSessionId.data.session_id);
+    localStorage.setItem('userId', newUserId.data.user_id);
+    localStorage.setItem('sessionId', newSessionId.data.session_id);
+  } catch (error) {
+    console.warn('Failed to get user and session:', error);
+  }
 };
