87174820574e8dd3d7319e3b97c645a56eeaa058
Théo <theo.nardin@cri-paris.org>
2025-09-26 14:49:32 +0200

    fix linter

--- src/stores/microlearning.ts
+++ src/stores/microlearning.ts
@@ -1,6 +1,4 @@
 import { defineStore } from 'pinia';
-import { ref, type Ref } from 'vue';
 
 export const useMicroLearningStore = defineStore('microlearning', ()=> {
-  const step: Ref<number> = ref(1);
 });
