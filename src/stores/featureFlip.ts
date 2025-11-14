571d348e1a75097c1a28f1ecdf5b38aeb4f5fcc0
Sandra Guerreiro <sandragjacinto@gmail.com>
2025-11-14 11:17:10 +0100

    feat(microlearning): featureflip, accessible only in DEV (#58)

--- src/stores/featureFlip.ts
+++ src/stores/featureFlip.ts
@@ -3,8 +3,10 @@ type FeatureFlip = {
   [key: string]: boolean;
 };
 export const useFeatureFlipStore = defineStore('featureFlip', () => {
+  const isDevEnvironment = import.meta.env.VITE_ENVIRONMENT === 'dev';
   const featureFlip: FeatureFlip = {
-    tutor: true
+    tutor: true,
+    microlearning: isDevEnvironment
   };
 
   const isFeatureEnabled = (feature: string) => {
