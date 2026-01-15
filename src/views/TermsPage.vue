fcebbc6bfd9f27d61c9812b3a241d81064820cc0
Sandra Guerreiro <sandragjacinto@gmail.com>
2026-01-15 11:48:39 +0100

    feat(terms):gets terms dyamically from github main (#74)

--- src/views/TermsPage.vue
+++ src/views/TermsPage.vue
@@ -1,353 +1,40 @@
 <script lang="ts" setup>
-const test = fetch('https://github.com/CyberCRI/WeLearn-api/blob/main/.env.example');
-console.log('test fetch', test);
+import i18n from '@/localisation/i18n';
+import { marked } from 'marked';
+import { onMounted, ref, watch, type Ref } from 'vue';
+import terms_en from '/public/terms_en.md?raw';
+
+const termsHtml: Ref<string | Promise<string>> = ref('Loading...');
+const fetchMarkdown = async (locale: string) => {
+  try {
+    const res = await fetch(
+      `https://raw.githubusercontent.com/CyberCRI/WeLearn-client/refs/heads/main/public/terms_${locale}.md`
+    );
+    if (!res.ok) throw new Error('Failed to fetch Markdown');
+    const md = await res.text();
+
+    termsHtml.value = marked(md);
+  } catch (err) {
+    termsHtml.value = marked(terms_en);
+    console.error(err);
+  }
+};
+
+onMounted(async () => {
+  fetchMarkdown(i18n.global.locale.value);
+});
+
+watch(
+  () => i18n.global.locale.value,
+  (newLocale: string) => {
+    fetchMarkdown(newLocale);
+  }
+);
 </script>
 
 <template>
   <div class="terms-page">
-    <h2 class="c16 c19" id="h.krb50ripy7h5"><span class="c8 c5"></span></h2>
-    <h2 class="c16" id="h.qz2faegvjd4b">
-      <span class="c5 c8">Politique de confidentialit&eacute; &ndash; WeLearn</span>
-    </h2>
-    <h3 class="c14" id="h.hons5mjkusew"><span class="c2">1. Qui sommes-nous ?</span></h3>
-    <p class="c1">
-      <span>WeLearn est une plateforme d&eacute;velopp&eacute;e par le </span
-      ><span class="c5">Learning Planet Institute (LPI)</span
-      ><span class="c0"
-        >, organisation &agrave; but non lucratif, dont l&rsquo;objectif est de soutenir
-        l&rsquo;enseignement et l&rsquo;apprentissage, notamment sur les enjeux de
-        durabilit&eacute;.</span
-      >
-    </p>
-    <p class="c1">
-      <span class="c5">Responsable du traitement<br /></span
-      ><span class="c0">Learning Planet Institute (LPI)</span>
-    </p>
-    <p class="c1">
-      <span class="c5"
-        >D&eacute;l&eacute;gu&eacute;e &agrave; la protection des donn&eacute;es (DPO)<br /></span
-      ><span>Email : </span><span class="c12 c11 c5">lamis.chamas@learningplanetinstitute.org</span>
-    </p>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h3 class="c14" id="h.n2wesay006br">
-      <span class="c2">2. Port&eacute;e de la pr&eacute;sente politique</span>
-    </h3>
-    <p class="c1">
-      <span
-        >La pr&eacute;sente politique de confidentialit&eacute; d&eacute;crit la mani&egrave;re dont
-        vos donn&eacute;es sont trait&eacute;es lorsque vous utilisez la plateforme WeLearn,
-        accessible </span
-      ><span>sans cr&eacute;ation de compte utilisateur</span><span class="c0">.</span>
-    </p>
-    <p class="c1"><span class="c0">Elle couvre les fonctionnalit&eacute;s suivantes :</span></p>
-    <ul class="c4 lst-kix_h0b6g3f9gktc-0 start">
-      <li class="c1 c3 li-bullet-0">
-        <span>recherche de r</span><span>essources</span><span class="c0">,</span>
-      </li>
-      <li class="c1 c3 li-bullet-0"><span class="c0">chat,</span></li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >g&eacute;n&eacute;ration de syllabus &agrave; partir de documents fournis par
-          l&rsquo;utilisateur,</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0"><span class="c0">formulaire de feedback.</span></li>
-    </ul>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h3 class="c14" id="h.cpo2bwfd4pbm">
-      <span class="c2">3. Principes g&eacute;n&eacute;raux</span>
-    </h3>
-    <p class="c1">
-      <span class="c0"
-        >Le Learning Planet Institute attache une importance particuli&egrave;re &agrave; la
-        protection des donn&eacute;es personnelles et applique les principes suivants :</span
-      >
-    </p>
-    <ul class="c4 lst-kix_p0xa26fi4yyu-0 start">
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >collecte limit&eacute;e aux donn&eacute;es strictement n&eacute;cessaires,</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0">absence de finalit&eacute; commerciale,</span>
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span>aucune revente des donn&eacute;es,</span>
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0">transparence sur les traitements effectu&eacute;s,</span>
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >h&eacute;bergement des donn&eacute;es au sein de l&rsquo;Union europ&eacute;enne
-          (principalement en France).</span
-        >
-      </li>
-    </ul>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h3 class="c14" id="h.gcidvnbb8eop">
-      <span class="c5 c17">4. Donn&eacute;es trait&eacute;es selon les </span
-      ><span class="c17 c5">fonctionnalit&eacute;s</span>
-    </h3>
-    <h4 class="c9" id="h.ltibzrp8p60e">
-      <span class="c11 c5">4.1 Recherche de </span><span class="c5 c11">ressources</span>
-    </h4>
-    <p class="c1"><span class="c0">Lorsque vous effectuez une recherche :</span></p>
-    <ul class="c4 lst-kix_nhtp181hved9-0 start">
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >les termes saisis sont trait&eacute;s de mani&egrave;re transitoire afin de produire une
-          r&eacute;ponse,</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0">aucune donn&eacute;e n&rsquo;est stock&eacute;e,</span>
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span>les requ&ecirc;tes ne sont pas conserv&eacute;es apr&egrave;s la r&eacute;ponse</span
-        ><span class="c0">.</span>
-      </li>
-    </ul>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h4 class="c9" id="h.pulkfk1dfosi">
-      <span class="c11 c5">4.2 </span><span class="c11 c5">Chat</span>
-    </h4>
-    <p class="c1"><span class="c0">Lorsque vous utilisez le chat :</span></p>
-    <ul class="c4 lst-kix_ljfflij55by0-0 start">
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >les messages saisis sont trait&eacute;s de mani&egrave;re transitoire afin de
-          g&eacute;n&eacute;rer une r&eacute;ponse,</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >les contenus du chat ne sont pas stock&eacute;s, ni r&eacute;utilis&eacute;s,</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span>aucun </span><span>entra&icirc;nement de mod&egrave;le d&rsquo;IA n&rsquo;est </span
-        ><span class="c0">effectu&eacute; &agrave; partir de ces &eacute;changes.</span>
-      </li>
-    </ul>
-    <p class="c1"><span class="c0">Il est rappel&eacute; que :</span></p>
-    <ul class="c4 lst-kix_vy6qi746k2c-0 start">
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >la plateforme n&rsquo;a pas pour objectif de collecter des donn&eacute;es &agrave;
-          caract&egrave;re personnel via le chat,</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span>toutefois, vous restez libre de saisir ou non des</span
-        ><span>&nbsp;informations personnelles</span><span>&nbsp;dans vos messages.</span>
-      </li>
-    </ul>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h4 class="c9" id="h.7xz5otc033ub">
-      <span class="c12 c11 c5"
-        >4.3 G&eacute;n&eacute;ration de syllabus &agrave; partir de documents</span
-      >
-    </h4>
-    <p class="c1">
-      <span>Dans le cadre de la g&eacute;n&eacute;ration de syllabus :</span>
-    </p>
-    <ul class="c4 lst-kix_g0cyq0hwvf5b-0 start">
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >vous pouvez t&eacute;l&eacute;verser des documents (PDF, texte, etc.),</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >ces documents sont trait&eacute;s uniquement pour produire la r&eacute;ponse
-          demand&eacute;e,</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0">les fichiers ne sont pas stock&eacute;s sur la plateforme,</span>
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span>ils sont transmis de mani&egrave;re transitoire</span><span>&nbsp;aux services</span
-        ><span class="c0"
-          >&nbsp;de traitement n&eacute;cessaires &agrave; la g&eacute;n&eacute;ration du
-          r&eacute;sultat, puis supprim&eacute;s.</span
-        >
-      </li>
-    </ul>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h4 class="c9" id="h.vqmc877ig2ds">
-      <span class="c12 c11 c5">4.4 Formulaire de feedback</span>
-    </h4>
-    <p class="c1">
-      <span>Lorsque vous r&eacute;pondez au formulaire de</span><span>&nbsp;feedback</span
-      ><span class="c0">, les donn&eacute;es suivantes sont collect&eacute;es :</span>
-    </p>
-    <ul class="c4 lst-kix_2malpqmhayzj-0 start">
-      <li class="c1 c3 li-bullet-0"><span>nom</span><span class="c0">,</span></li>
-      <li class="c1 c3 li-bullet-0"><span class="c0">adresse e-mail,</span></li>
-      <li class="c1 c3 li-bullet-0"><span class="c0">profession.</span></li>
-    </ul>
-    <p class="c1"><span class="c0">Ces champs sont obligatoires afin de :</span></p>
-    <ul class="c4 lst-kix_uho7399md0ur-0 start">
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >garantir que les retours proviennent bien du public cible (enseignement
-          sup&eacute;rieur),</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0">limiter le bruit et les r&eacute;ponses non pertinentes,</span>
-      </li>
-      <li class="c1 c3 li-bullet-0"><span>permettre un suivi qualitatif des retours.</span></li>
-    </ul>
-    <p class="c1">
-      <span class="c5">Finalit&eacute;s du traitement :</span>
-    </p>
-    <ul class="c4 lst-kix_pwv5pm2u1lqe-0 start">
-      <li class="c1 c3 li-bullet-0"><span class="c0">mesure de satisfaction,</span></li>
-      <li class="c1 c3 li-bullet-0"><span class="c0">am&eacute;lioration du produit,</span></li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0">&eacute;ventuel suivi des retours exprim&eacute;s.</span>
-      </li>
-    </ul>
-    <p class="c1">
-      <span
-        >Les r&eacute;ponses au formulaire ne sont accessibles qu&rsquo;aux &eacute;quipes internes
-        du projet WeLearn.</span
-      >
-    </p>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h3 class="c14" id="h.fizmzjxy6w4m">
-      <span class="c2">5. Base l&eacute;gale des traitements</span>
-    </h3>
-    <p class="c1">
-      <span class="c0">Les traitements d&eacute;crits ci-dessus reposent :</span>
-    </p>
-    <ul class="c4 lst-kix_3pd3qylqhzjx-0 start">
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >sur l&rsquo;int&eacute;r&ecirc;t l&eacute;gitime du Learning Planet Institute &agrave;
-          am&eacute;liorer un outil p&eacute;dagogique non commercial,</span
-        >
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span
-          >et, lorsque requis (formulaire de feedback), sur votre consentement, exprim&eacute;
-          notamment via les cases &agrave; cocher pr&eacute;vues &agrave; cet effet</span
-        ><span>.</span>
-      </li>
-    </ul>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h3 class="c14" id="h.9l280y39e23v">
-      <span class="c2">6. Destinataires des donn&eacute;es</span>
-    </h3>
-    <p class="c1">
-      <span class="c0"
-        >Les donn&eacute;es collect&eacute;es sont destin&eacute;es exclusivement :</span
-      >
-    </p>
-    <ul class="c4 lst-kix_vw1qellxr2oa-0 start">
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0"
-          >aux &eacute;quipes internes du Learning Planet Institute impliqu&eacute;es dans le projet
-          WeLearn.</span
-        >
-      </li>
-    </ul>
-    <p class="c1">
-      <span class="c0"
-        >Aucune donn&eacute;e n&rsquo;est transmise &agrave; des tiers &agrave; des fins
-        commerciales.</span
-      >
-    </p>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h3 class="c14" id="h.db7de18ctxn1">
-      <span class="c17 c5">7. </span
-      ><span class="c17 c5">S&eacute;curit&eacute; et h&eacute;bergement</span>
-    </h3>
-    <p class="c1">
-      <span
-        >Des mesures techniques et organisationnelles appropri&eacute;es sont mises en place afin de
-        garantir </span
-      ><span class="c0">la s&eacute;curit&eacute; des donn&eacute;es.</span>
-    </p>
-    <p class="c1">
-      <span class="c0"
-        >Les infrastructures utilis&eacute;es sont h&eacute;berg&eacute;es au sein de l&rsquo;Union
-        europ&eacute;enne, principalement en France.</span
-      >
-    </p>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h3 class="c14" id="h.4sd13jonpvmq">
-      <span class="c2">8. Dur&eacute;es de conservation</span>
-    </h3>
-    <p class="c1">
-      <span
-        >Les donn&eacute;es issues du formulaire de feedback sont conserv&eacute;es pendant u</span
-      ><span>ne dur&eacute;e proportionn&eacute;e </span
-      ><span class="c0"
-        >aux finalit&eacute;s poursuivies, puis supprim&eacute;es ou anonymis&eacute;es.</span
-      >
-    </p>
-    <p class="c1">
-      <span class="c0"
-        >Les donn&eacute;es trait&eacute;es de mani&egrave;re transitoire (recherche, chat,
-        documents t&eacute;l&eacute;vers&eacute;s) ne sont pas conserv&eacute;es.</span
-      >
-    </p>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h3 class="c14" id="h.ie7kbdq3dvj"><span class="c2">9. Vos droits</span></h3>
-    <p class="c1">
-      <span
-        >Conform&eacute;ment &agrave; la r&eacute;glementation en vigueur, vous disposez des droits
-        suivants</span
-      ><span class="c0">&nbsp;:</span>
-    </p>
-    <ul class="c4 lst-kix_ngawrgofufje-0 start">
-      <li class="c1 c3 li-bullet-0"><span class="c0">droit d&rsquo;acc&egrave;s,</span></li>
-      <li class="c1 c3 li-bullet-0"><span class="c0">droit de rectification,</span></li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0">droit &agrave; l&rsquo;effacement,</span>
-      </li>
-      <li class="c1 c3 li-bullet-0">
-        <span class="c0">droit &agrave; la limitation du traitement,</span>
-      </li>
-      <li class="c1 c3 li-bullet-0"><span class="c0">droit d&rsquo;opposition,</span></li>
-      <li class="c1 c3 li-bullet-0">
-        <span>droit &agrave; la portabilit&eacute; de vos donn&eacute;es lorsque applicable.</span>
-      </li>
-    </ul>
-    <p class="c1">
-      <span
-        >Pour exercer vos droits, vous pouvez contacter la DPO du Learning Planet Institute &agrave;
-        l&rsquo;adresse suivante :<br /></span
-      ><span class="c12 c11 c5">lamis.chamas@learningplanetinstitute.org</span>
-    </p>
-    <hr />
-    <p class="c13"><span class="c0"></span></p>
-    <h3 class="c14" id="h.f76wr8yoxqiq">
-      <span class="c2">10. &Eacute;volution de la politique</span>
-    </h3>
-    <p class="c1">
-      <span class="c0"
-        >La pr&eacute;sente politique pourra &ecirc;tre mise &agrave; jour afin de refl&eacute;ter
-        les &eacute;volutions de la plateforme ou du cadre r&eacute;glementaire. La date de
-        derni&egrave;re mise &agrave; jour sera indiqu&eacute;e.</span
-      >
-    </p>
+    <div v-html="termsHtml"></div>
   </div>
 </template>
 
@@ -356,30 +43,5 @@ console.log('test fetch', test);
   max-width: 70%;
   margin: 2rem auto;
   padding: 0 1rem;
-}
-
-h1 {
-  font-size: 2rem;
-  margin-bottom: 1rem;
-  font-weight: 600;
-}
-
-h2 {
-  font-size: 1.5rem;
-  margin-top: 2rem;
-  margin-bottom: 1rem;
-  margin-bottom: 1rem;
-  font-weight: 600;
-  font-weight: 600;
-}
-
-h3 {
-  font-size: 1.25rem;
-  margin-top: 2rem;
-  margin-top: 2rem;
-  margin-bottom: 1rem;
-  margin-bottom: 1rem;
-  font-weight: 600;
-  nt-weight: 600;
 }
 </style>
