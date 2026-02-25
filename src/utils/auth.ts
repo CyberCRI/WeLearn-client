000be91920a620e8b873d28e6c11a073f686a602
Sandra Guerreiro <sandragjacinto@gmail.com>
2026-02-25 15:07:09 +0100

    feat(auth): makes sure correct session info is registered each time (#93)
    * feat(auth): makes sure correct session info is registered each time
    
    * user id is now really passed

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,40 +4,48 @@ export const getUserAndSession = async (
   userId?: string,
   sessionId?: string,
   referer?: string
-): { userId: string; sessionId: string } => {
+): Promise<{ userId: string; sessionId: string }> => {
   try {
-    if (userId && sessionId) return { userId, sessionId };
-
     if (!userId && sessionId) {
       localStorage.removeItem('sessionId');
     }
 
-    let newUserId = userId;
-    let newSessionId = sessionId;
+    const newUserId = await getUserId(userId, referer);
+    const newSessionId = await getSessionId(newUserId, sessionId, referer);
 
-    if (!newUserId) {
-      const parts: string[] = [];
-      if (userId) parts.push(`user_id=${encodeURIComponent(userId)}`);
-      if (referer) parts.push(`referer=${encodeURIComponent(referer)}`);
-
-      const query = parts.length ? `?${parts.join('&')}` : '';
-      const respUserId = await postAxios(`/user/user${query}`);
-      newUserId = respUserId.data.user_id;
+    if (!newUserId || !newSessionId) {
+      throw new Error('Failed to retrieve user ID or session ID');
     }
 
-    if (newUserId) {
-      const parts: string[] = [];
-      parts.push(`user_id=${encodeURIComponent(newUserId)}`);
-      if (referer) parts.push(`referer=${encodeURIComponent(referer)}`);
-
-      const query = `?${parts.join('&')}`;
-      const respSessionId = await postAxios(`/user/session${query}`);
-
-      newSessionId = respSessionId.data.session_id;
-    }
     return { userId: newUserId, sessionId: newSessionId };
   } catch (error) {
     console.warn('Failed to get user and session:', error);
     throw error;
   }
+};
+
+const getUserId = async (userId?: string, referer?: string): Promise<string> => {
+  const parts: string[] = [];
+
+  if (userId) parts.push(`user_id=${encodeURIComponent(userId)}`);
+  if (referer) parts.push(`referer=${encodeURIComponent(referer)}`);
+
+  const query = parts.length ? `?${parts.join('&')}` : '';
+  const respUserId = await postAxios(`/user/user${query}`);
+  return respUserId.data.user_id;
+};
+
+const getSessionId = async (
+  userId: string,
+  sessionId?: string,
+  referer?: string
+): Promise<string> => {
+  const parts: string[] = [];
+  parts.push(`user_id=${encodeURIComponent(userId)}`);
+  if (sessionId) parts.push(`session_id=${encodeURIComponent(sessionId)}`);
+  if (referer) parts.push(`referer=${encodeURIComponent(referer)}`);
+
+  const query = `?${parts.join('&')}`;
+  const respSessionId = await postAxios(`/user/session${query}`);
+  return respSessionId.data.session_id;
 };
