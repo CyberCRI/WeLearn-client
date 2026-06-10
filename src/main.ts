46d070cebb1b5257b674a58dcc68b86f87c7b2c8
Sandra Guerreiro <sandragjacinto@gmail.com>
2026-06-10 15:33:41 +0200

    feat(metrics): adds mixpanel (#125)

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,16 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import i18n from './localisation/i18n';
+//Import Mixpanel SDK (only required for npm and yarn)
+import mixpanel from 'mixpanel-browser';
 
+mixpanel.init(import.meta.env.VITE_MIXPANEL_TOKEN, {
+  debug: import.meta.env.VITE_ENVIRONMENT === 'dev', // Enable debug mode for development
+  track_pageview: true,
+  persistence: 'localStorage',
+  record_sessions_percent: 100, //records 100% of all sessions
+  record_heatmap_data: true
+});
 const pinia = createPinia();
 const app = createApp(App);
 
