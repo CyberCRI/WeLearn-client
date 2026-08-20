8ebcda1382d2167e50a7a1e166ff411243f2e9b4
Sandra Guerreiro <sandragjacinto@gmail.com>
2026-08-20 12:21:20 +0200

    fix lint :facepalm:

--- src/components/SourcesListComponent.vue
+++ src/components/SourcesListComponent.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
 import type { Document } from '@/types';
 import Card from '@/components/CardComponent.vue';
 import SimpleCard from '@/components/CardSimpleComponent.vue';
